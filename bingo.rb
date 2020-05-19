e82538be25445365b66c33aa5d74bdf1bb608df7
catt <catherine.chun@yahoo.com>
2020-05-19 10:44:57 -0700

    Made card into PDF

--- bingo.rb
+++ bingo.rb
@@ -1,3 +1,5 @@
+require "prawn"
+
 def align(num)
   if num < 10
     " #{num}"
@@ -12,6 +14,8 @@ n_col = (31..45).to_a.sample(4)
 g_col = (46..60).to_a.sample(5)
 o_col = (61..75).to_a.sample(5)
 
+n_col.insert(2, "F")
+
 bingo_card = <<-BINGO
 +-----+-----+-----+-----+-----+
 |  B  |  I  |  N  |  G  |  O  |
@@ -22,10 +26,59 @@ bingo_card = <<-BINGO
 +-----+-----+-----+-----+-----+
 | #{align(b_col[2])}  | #{align(i_col[2])}  |  F  | #{align(g_col[2])}  | #{align(o_col[2])}  |
 +-----+-----+-----+-----+-----+
-| #{align(b_col[3])}  | #{align(i_col[3])}  | #{align(n_col[2])}  | #{align(g_col[3])}  | #{align(o_col[3])}  |
+| #{align(b_col[3])}  | #{align(i_col[3])}  | #{align(n_col[3])}  | #{align(g_col[3])}  | #{align(o_col[3])}  |
 +-----+-----+-----+-----+-----+
-| #{align(b_col[4])}  | #{align(i_col[4])}  | #{align(n_col[3])}  | #{align(g_col[4])}  | #{align(o_col[4])}  |
+| #{align(b_col[4])}  | #{align(i_col[4])}  | #{align(n_col[4])}  | #{align(g_col[4])}  | #{align(o_col[4])}  |
 +-----+-----+-----+-----+-----+
 BINGO
 
 puts bingo_card
+
+header = ["B", "I", "N", "G", "O"]
+
+Prawn::Document.generate("bingo.pdf") do
+  define_grid(columns: 5, rows: 6)
+
+  header.each_with_index do |char, i|
+    grid(0, i).bounding_box do
+      stroke_bounds
+      text char, align: :center, valign: :center, size: 50, style: :bold
+    end
+  end
+
+  b_col.each_with_index do |num, i|
+    grid((i+1), 0).bounding_box do
+      stroke_bounds
+      text num.to_s, align: :center, valign: :center, size: 50
+    end
+  end
+
+  i_col.each_with_index do |num, i|
+    grid((i+1), 1).bounding_box do
+      stroke_bounds
+      text num.to_s, align: :center, valign: :center, size: 50
+    end
+  end
+
+  n_col.each_with_index do |num, i|
+    grid((i+1), 2).bounding_box do
+      stroke_bounds
+      text num.to_s, align: :center, valign: :center, size: 50
+    end
+  end
+
+  g_col.each_with_index do |num, i|
+    grid((i+1), 3).bounding_box do
+      stroke_bounds
+      text num.to_s, align: :center, valign: :center, size: 50
+    end
+  end
+
+  o_col.each_with_index do |num, i|
+    grid((i+1), 4).bounding_box do
+      stroke_bounds
+      text num.to_s, align: :center, valign: :center, size: 50
+    end
+  end
+
+end
